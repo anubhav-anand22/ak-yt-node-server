76198b016bd087a5caed8bb171779699077646ab
anubhav anand <anubhav.ak22@gmail.com>
2024-04-07 01:00:09 +0530

    try catch

--- app.js
+++ app.js
@@ -8,29 +8,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.get("/api/download-vid", async (req, res) => {
-  const id = req.query?.id || "";
-  if (!id) return res.status(400).send("Id is required");
+  try {
+    const id = req.query?.id || "";
+    if (!id) return res.status(400).send("Id is required");
 
-  const url = "http://www.youtube.com/watch?v=" + id;
+    const url = "http://www.youtube.com/watch?v=" + id;
 
-  const info = await ytdl.getInfo(url);
+    const info = await ytdl.getInfo(url);
 
-  const format = info.formats.find((e) => e.itag === 140 || e.itag === 139);
+    const format = info.formats.find((e) => e.itag === 140 || e.itag === 139);
 
-  if (!format) return res.status(404).send("No format found");
+    if (!format) return res.status(404).send("No format found");
 
-  res.setHeader("Content-Type", format.mimeType);
-  res.setHeader("Content-Length", format.contentLength);
-  res.setHeader(
-    "Content-Disposition",
-    `attachment; filename="${sanitizeTxt(info.videoDetails.title)}.m4a"`
-  );
+    res.setHeader("Content-Type", format.mimeType);
+    res.setHeader("Content-Length", format.contentLength);
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="${sanitizeTxt(info.videoDetails.title)}.m4a"`
+    );
 
-  ytdl
-    .downloadFromInfo(info, {
-      format,
-    })
-    .pipe(res);
+    ytdl
+      .downloadFromInfo(info, {
+        format,
+      })
+      .pipe(res);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Something went wrong");
+    console.log(e);
+  }
 });
 
 const sanitizeTxt = (str = "") => {
