44e56ce96bc8b64b5e1f5bc72bfc4b3f2d92b9b3
anubhav anand <anubhav.ak22@gmail.com>
2024-04-07 00:51:34 +0530

    init

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/api/download-vid", async (req, res) => {
+  const id = req.query?.id || "";
+  if (!id) return res.status(400).send("Id is required");
+
+  const url = "http://www.youtube.com/watch?v=" + id;
+
+  const info = await ytdl.getInfo(url);
+
+  const format = info.formats.find((e) => e.itag === 140 || e.itag === 139);
+
+  if (!format) return res.status(404).send("No format found");
+
+  res.setHeader("Content-Type", format.mimeType);
+  res.setHeader("Content-Length", format.contentLength);
+  res.setHeader(
+    "Content-Disposition",
+    `attachment; filename="${sanitizeTxt(info.videoDetails.title)}.m4a"`
+  );
+
+  ytdl
+    .downloadFromInfo(info, {
+      format,
+    })
+    .pipe(res);
+});
+
+const sanitizeTxt = (str = "") => {
+  let out = "";
+  const allowed =
+    "abcdfghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ";
+  for (let i of allowed) {
+    if (str.includes(i)) out += i;
+  }
+  return out;
+};
+
+app.listen(port, () => {
+  console.log("Server is up on port: " + port);
+});
